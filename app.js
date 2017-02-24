9a9665a5c5f3c8d388cb53f6fae85b5d0e68dd97
Amine Rifi <amineprog@gmail.com>
2017-02-24 16:43:24 +0000

    maj

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+Genre=require('./models/genre');
 mongoose.connect('mongodb://localhost/bookstore');
 var db = mongoose.connection;
-
+var data=[];
 app.get('/',function(req,res){
     res.send("Salam !");
 });
 
-app.get('/api/all',function(req,res){
-    var data=[];
-    data.push("amine","ali");
-    res.send(data);
+app.get('/api/genres',function(req,res){      
+    Genre.getGenres(function(err,genres){
+        if(err){
+            throw err;
+        }
+        res.json(genres);
+    });    
 });
+
+app.get('/api/genres',function(req,res){
+
+});
+
 app.listen(3000);
 console.log('Running on port 3000...');
---
+++ models/genre.js
@@ -0,0 +1,22 @@
+var mongoose=require("mongoose");
+
+// Genre Schema
+
+var genreSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    create_date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+var Genre=module.exports=mongoose.Model('Genres',genreSchema);
+
+// get Genres 
+
+module.exports.getGenres=function(callBack,limit){
+Genre.find(callBack).limit(limit);
+}
